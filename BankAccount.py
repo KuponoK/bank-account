7a9acc4fc9adfb42b78258c3ab06c926c79681d9
Kupono Kealiinohomoku <kuponokealiinohomoku@AthinaspleWatch.localdomain>
2020-11-09 15:12:48 -0800

    added print_receipt method
    also forgot to add private variables

--- BankAccount.py
+++ BankAccount.py
@@ -2,30 +2,34 @@
 class BankAccount:
 
     def __init__(self, full_name, account_number, routing_number, balance):
-        self.full_name = full_name
-        self.account_number = account_number
-        self.routing_number = routing_number
-        self.balance = balance
+        self.__full_name = full_name
+        self.__account_number = account_number
+        self.__routing_number = routing_number
+        self.__balance = balance
     
 # deposit function 
     def deposit(self, amount):
-        self.balance += amount
+        self.__balance += amount
 
 # withdraw function
     def withdraw(self, amount):
-        if (amount <= self.balance):
-            self.balance -= amount
+        if (amount <= self.__balance):
+            self.__balance -= amount
             print(f'Amount Withdrawn: {amount}')
         else:
             print('Insufficient funds')
-            self.balance -= 10
+            self.__balance -= 10
 
 # get_balance method
     def get_balance(self):
-        print(f'You have a balance of {self.balance}')
-        return self.balance
+        print(f'You have a balance of {self.__balance}')
+        return self.__balance
 
 # add_intrest method
     def add_interest(self):
-        interest = self.balance * 0.00083
-        self.balance += round(interest, 2)
+        interest = self.__balance * 0.00083
+        self.__balance += round(interest, 2)
+
+# print_receipt method
+    def print_receipt(self):
+        print(f'{self__.full_name}\n Account No: {self.__account_number}\n Routing No: {self.__routing_number}\n Balance: {self.__balance}')
